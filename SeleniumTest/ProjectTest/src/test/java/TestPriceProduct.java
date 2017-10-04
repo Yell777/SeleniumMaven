92a9c4e4753ce955dee7feb76fa5dde46af260d2
Mike <30903250+Yell777@users.noreply.github.com>
2017-10-04 19:38:17 +0300

    Создан тест на проверку товара

---
+++ SeleniumTest/ProjectTest/src/test/java/TestPriceProduct.java
@@ -0,0 +1,20 @@
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+
+/**
+ * Created by User on 04.10.2017.
+ */
+public class TestPriceProduct extends TestBase {
+    @Test
+    public void testSticker () {
+        driver.get("http://localhost/litecart/en/");
+     
+
+
+    }
+
+}
+
--- SeleniumTest/ProjectTest/src/test/java/testListDuckStick.java
+++ SeleniumTest/ProjectTest/src/test/java/testListDuckStick.java
@@ -9,19 +9,19 @@ import java.util.List;
 public class testListDuckStick extends TestBase {
 
     @Test
-    public void testSticker () {
-        driver.get("http://localhost/litecart/en/");
-        List<WebElement> duckList = driver.findElements(By.cssSelector("a.link "));
-        for (int i = 0; i < duckList.size() ; i++) {
-            try {
-                duckList.get(i).findElement(By.cssSelector("div[class^=sticker]"));
-            }catch (Exception ex){
-                System.out.println("Эелемент не найден ");
-            }
+public void testSticker () {
+    driver.get("http://localhost/litecart/en/");
+    List<WebElement> duckList = driver.findElements(By.cssSelector("a.link "));
+    for (int i = 0; i < duckList.size() ; i++) {
+        try {
+            duckList.get(i).findElement(By.cssSelector("div[class^=sticker]"));
+        }catch (Exception ex){
+            System.out.println("Эелемент не найден ");
+        }
 //            Assert.assertTrue(isElementPresent(By.cssSelector("[class^=stick]")));
 
-            }
-
-        }
-
     }
+
+}
+
+}

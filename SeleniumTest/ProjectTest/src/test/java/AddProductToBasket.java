59d50542c095e12e8d8456514bf2174857e08a96
Mike <30903250+Yell777@users.noreply.github.com>
2017-10-12 16:37:21 +0300

    Сделал половину 13-го задания

--- SeleniumTest/ProjectTest/src/test/java/AddProductToBasket.java
+++ SeleniumTest/ProjectTest/src/test/java/AddProductToBasket.java
@@ -1,8 +1,10 @@
+import org.apache.xpath.SourceTree;
 import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.List;
@@ -17,18 +19,23 @@ public class AddProductToBasket extends TestBase {
         wait = new WebDriverWait(driver,30);
         driver.get("http://localhost/litecart/en/");
         WebElement a ;
+
+        /*Добавляю и проверяю счетчки корзины */
+
         List<WebElement> list = driver.findElements(By.xpath("//div[@id='box-most-popular']//ul/li"));
-        for (int i = 0; i < 3 ; i++) {
+        for (int i = 1; i <= 3 ; i++) {
             list = driver.findElements(By.xpath("//div[@id='box-most-popular']//ul/li"));
             list.get(i).click();
+            if (isElementPresent(By.xpath("//div[@class='buy_now']//td[@class='options']"))){
+                driver.findElement(By.xpath("//div[@class='buy_now']//td[@class='options']")).click();
+                driver.findElement(By.xpath("//div[@class='buy_now']//td[@class='options']//option[@value='Small']")).click();
+            }
             driver.findElement(By.xpath("//button[@name='add_cart_product']")).click();
-            wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div[@id='cart-wrapper']//span[@class='quantity']")));
+            a = driver.findElement(By.className("quantity"));
+            wait.until(ExpectedConditions.attributeContains(a , "textContent" ,""+i));
             String s = driver.findElement(By.xpath("//div[@id='cart-wrapper']//span[@class='quantity']")).getAttribute("textContent");
-            System.out.println(s);
-//            Assert.assertTrue("Счетчик не обновлен",Integer.parseInt(s)== i+1);
-
-
-
+            Assert.assertTrue("Счетчик не обновлен",Integer.parseInt(s)== i);
+            driver.get("http://localhost/litecart/en/");
         }
 
 

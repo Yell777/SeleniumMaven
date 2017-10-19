690d7830fd2c7b240267eeae210f31515bd0b665
Mike <30903250+Yell777@users.noreply.github.com>
2017-10-19 18:52:33 +0300

    Задание 15

--- SeleniumTest/ProjectTest/src/test/java/MyFirstTest.java
+++ SeleniumTest/ProjectTest/src/test/java/MyFirstTest.java
@@ -5,7 +5,13 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by User on 02.10.2017.
@@ -15,8 +21,11 @@ public class MyFirstTest {
     private WebDriver driver;
     private WebDriverWait wait;
     @Before
-    public void start (){
-        driver = new ChromeDriver();
+    public void start () throws MalformedURLException {
+        DesiredCapabilities capability = DesiredCapabilities.firefox();
+        capability.setBrowserName("internet explorer");
+        capability.setAcceptInsecureCerts(false);
+        driver = new RemoteWebDriver(new URL("http://192.168.5.22:4444/wd/hub"),capability);
         wait = new WebDriverWait(driver,10);
     }
     @Test
